9b91a00a7c5450b685de00c37cc2820e51211f2e
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-12-28 00:46:13 +0600

    basic-files-added

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,51 @@
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <div>
+            <Navbar
+                fluid={true}
+                rounded={true}
+            >
+                <Navbar.Brand href="https://flowbite.com/">
+                    <img
+                        src="https://flowbite.com/docs/images/logo.svg"
+                        className="mr-3 h-6 sm:h-9"
+                        alt="Flowbite Logo"
+                    />
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                        Flowbite
+                    </span>
+                </Navbar.Brand>
+                <div className="flex md:order-2">
+                    <Dropdown
+                        arrowIcon={false}
+                        inline={true}
+                        label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
+                    >
+                    </Dropdown>
+                    <Navbar.Toggle />
+                </div>
+                <Navbar.Collapse>
+                    <Link to='/home' active="true">Home</Link>
+                    <Navbar.Link href="/navbars">
+                        About
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Services
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Pricing
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Contact
+                    </Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+};
+
+export default NavBar;
